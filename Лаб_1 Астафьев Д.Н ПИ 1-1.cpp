150e38b64fffcd259a13e9848df985dbc37a7943
Silver-g <84073740+Silver-g@users.noreply.github.com>
2021-06-03 15:33:37 +0300

    Create Лаб_1 Астафьев Д.Н ПИ 1-1.cpp

---
+++ Лаб_1 Астафьев Д.Н ПИ 1-1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int raz = 10;
+    int mass[raz] = { 15,20,45,4,551,641,74,1,9,25 };
+    for (int i = 0; i < raz; i++)
+    {
+        cout << mass[i] << ' ';
+        if (mass[i] % 2 == 0)
+                cout << 0 << endl;
+        else
+            cout << mass[i] << endl;
+    }
+    return 0;
+}
