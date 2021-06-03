4163f6fa351fcb77cd96e1484c4a6e1caf49916e
Silver-g <84073740+Silver-g@users.noreply.github.com>
2021-06-03 15:33:42 +0300

    Create Лаб_2 Астафьев ПИ 1-1.cpp

---
+++ Лаб_2 Астафьев ПИ 1-1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int i;
+	string s;
+	char c;
+	cin >> c;
+		cin >> s;
+	bool naideno = false;
+	int nomer = 0;
+	for (i = 0; i < s.length(); i++) {
+		if (s[i] == c) {
+			nomer = i;
+			naideno = true;
+			break;
+		}
+	}
+	if (naideno) {
+		cout << nomer;
+	}
+	else {
+		cout << "NO !";
+	}
+	return 0;
+}
