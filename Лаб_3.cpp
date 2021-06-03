dffbce7b56a9a1dc3b8d99dc8b4b977100baf3d4
Silver-g <84073740+Silver-g@users.noreply.github.com>
2021-06-03 15:33:46 +0300

    Create Лаб_3.cpp

---
+++ Лаб_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int matrica[3][3];
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++) {
+			cout << "a" << i << j << endl;
+			cin >> matrica[i][j];
+		}
+	}
+	int det = matrica[0][0] * matrica[1][1] * matrica[2][2]
+		- matrica[0][0] * matrica[1][2] * matrica[2][1]
+		- matrica[0][1] * matrica[1][0] * matrica[2][2]
+		+ matrica[0][1] * matrica[1][2] * matrica[2][0]
+		+ matrica[0][2] * matrica[1][0] * matrica[2][1]
+		- matrica[0][2] * matrica[1][1] * matrica[2][0];
+	cout << "Детерминанта : " << det;
+	return 0;
+}
